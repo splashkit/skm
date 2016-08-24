20ef891bb170c375ee78088d96b28592bd3d63f1
Jake Renzella <j@kerenzella.com>
2016-08-24 23:59:16 +1000

    QUALITY: Remove unnecessary logs

--- src/index.js
+++ src/index.js
@@ -17,25 +17,24 @@ if (isMacOS) {
 
 app.on('ready', function () {
 
-  logger.info('SKM is ready...');
+  logger.info('SplashKit Manager\n');
 
-  // TODO: check if development or runtime.
+  // check if development or runtime.
   const cmdName = process.argv[argPos]
 
   //args will be left with everything after the command itself.
   const args = process.argv.slice(argPos + 1);
 
   var useCLI = cmdName != null
-  logger.info("are we using the CLI?: " + useCLI + " " + argPos)
 
   if (useCLI) {
+    logger.info(`using SKM CLI with ${argPos}`)
     let callback = function (err, data) { };
     cli.execute(cmdName, args, callback);
   }
   else {
-    {
-      //TODO: Open Window
-    }
+    //TODO: Open Window
   }
+  
   app.quit();
 });
