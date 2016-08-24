a4b6714c15891a683967242ea3a3c7e6100afb65
Jake Renzella <j@kerenzella.com>
2016-08-24 14:41:59 +1000

    FIX: Use fs Sync methods

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,7 +1,6 @@
 const utils = require('../utilities');
 const fs = require('fs');
 const init = require('./init');
-const jsonminify = require("jsonminify");
 const winston = require('winston-color');
 
 const preExecuteOnCLI = function() {
@@ -9,45 +8,37 @@ const preExecuteOnCLI = function() {
     return [];
 }
 
-// I Think there is a concurrency issue here.
-// 3am update I don't think there is a concurrency issue at all...
-function readSplashkitFile(args, callback) {
-  if (utils.isMacOS) {
-    fs.readFile('./.splashkit', 'utf8', function (err, data) {
-      if (err) {
-        return winston.error("Can't read file");
-      }
-      callback(data)
-    });
-  }
-}
-
 const execute = function(args, callback) {
   //check if we need to init or not and init if we need to.
-  if (!utils.isSplashkit()) {
-    winston.info("not found, initing directory with given language " + args[0])
+  let isDotFile = utils.isSplashkit('./')
+
+  if (!isDotFile && args.length > 0) {
+    return winston.error(`no args given to an non splashkit folder splashkit folder.`)
+  }
+  if (!isDotFile)
+  {
+    winston.info(`initing directory with language ${args[0]}`)
     init.execute(args)
   }
 
+
   //read the .splashkit file
-  readSplashkitFile(args, function(data) {
+  const splashKitData = utils.readDotSplashkit()
 
-    const splashKitData = JSON.parse(JSON.minify(data))
-    //now we have a init'd directory, so check its status
+  //now we have a init'd directory, so check its status
+  if (args.length > 0 && splashKitData.language != args[0]) {
+    return winston.error(`can\'t create ${args[0]} in a ${splashKitData.language} splashkit folder.`)
+  }
 
-    if (args.length > 0 && splashKitData.language != args[0]) {
-      return winston.error(`can\'t create ${args[0]} in a ${splashKitData.language} splashkit folder.`)
-    }
+  if (splashKitData.status != 'initialised') {
+    return winston.error(`can\'t create Spalshkit in a ${splashKitData.status} splashkit folder.`)
+  }
 
-    if (splashKitData.status != 'initialised') {
-      return winston.error(`can\'t create Spalshkit in a ${splashKitData.status} splashkit folder.`)
-    }
+  winston.info(`initialised folder found, creating: ${splashKitData.language} folder structure.`)
+  splashKitData.status = "created"
+  utils.writeDotSplashkit(splashKitData)
+  // TODO: Create folder for correct langauge in splashKitData.language
 
-      winston.info(`initialised folder found, creating: ${splashKitData.language} folder structure.`)
-      splashKitData.status = "created"
-      utils.writeDotSplashkit(splashKitData)
-      // TODO: Create folder for correct langauge in splashKitData.language
-  })
 }
 
 module.exports = {
