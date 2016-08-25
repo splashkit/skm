499f9f7411e249bc295e825e9b1d6ab82ddf5094
Jake Renzella <j@kerenzella.com>
2016-08-25 15:46:29 +1000

    FEATURE: Add initial implementation of minimist

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const app = require('electron').app;
 const cli = require('./cli');
-const logger = require('winston-color');
+
+// const gui = require('./gui');
 
 let argPos;
 
 console.log(process.env.NODE_ENV)
 process.env.NODE_ENV == 'development' ? argPos = 2 : argPos = 1
+
+const argv = require('minimist')(process.argv.slice(argPos))
+console.log((process.argv.slice(argPos)))
+console.log(argv)
 
 const isMacOS = (process.platform === 'darwin');
 
@@ -17,24 +22,23 @@ if (isMacOS) {
 
 app.on('ready', function () {
 
-  logger.info('SplashKit Manager\n');
-
   // check if development or runtime.
   const cmdName = process.argv[argPos]
 
   //args will be left with everything after the command itself.
   const args = process.argv.slice(argPos + 1);
 
-  var useCLI = cmdName != null
+  let useCLI = cmdName != null
 
   if (useCLI) {
-    logger.info(`using SKM CLI with ${argPos}`)
-    let callback = function (err, data) { };
+    let callback = function (err, data) {
+
+    };
     cli.execute(cmdName, args, callback);
   }
   else {
     //TODO: Open Window
   }
-  
+
   app.quit();
 });
