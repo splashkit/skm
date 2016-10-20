4abea9ee0c73693bb5f49b6d433592da81f613ed
Jake Renzella <j@kerenzella.com>
2016-10-20 12:30:17 +1100

    FIX: Add executable bash

--- skm-install.sh
+++ skm-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ `uname` == Darwin; ]] then
+if [[ `uname` == Darwin ]]; then
   bash <(curl -s https://raw.githubusercontent.com/jakerenzella/skm/develop/mac-build/mac-install.sh)
 fi
 
