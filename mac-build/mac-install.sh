8a70492accc2cfb76bf7ce672dbbbb952a6e724c
Jake Renzella <j@kerenzella.com>
2016-10-20 15:05:49 +1100

    QUALITY: Clean up skm folder

--- mac-build/mac-install.sh
+++ mac-build/mac-install.sh
@@ -2,27 +2,31 @@
 GIT_SKM_REPO=https://github.com/jakerenzella/skm
 GIT_MACOS_REPO=https://github.com/splashkit/splashkit-macos
 
-INSTALL_MACOS_PATH=~/.splashkit/splashkit-macos
-INSTALL_SKM_PATH=~/.splashkit/skm
+HOME_PATH=~
+FOLDER_PATH="${HOME_PATH}/.splashkit"
+INSTALL_PATH="${FOLDER_PATH}/splashkit-macos"
+INSTALL_SKM_PATH="${FOLDER_PATH}/skm"
 
 # Check for clang
-if which clang++ >/dev/null; then
-  CLANG_EXISTS=true
-else
-  "SplashKit will install, but we can not find a clang compiler. Please run Xcode developer command."
-fi
+
+command -v clang >/dev/null 2>&1 || { echo "SplashKit will install, but we can not find a clang compiler. Please run Xcode developer command."}
+# if which clang++ >/dev/null; then
+#   CLANG_EXISTS=true
+# else
+#   "SplashKit will install, but we can not find a clang compiler. Please run Xcode developer command."
+# fi
 
 # Clone the repos if git is found.
 if which git >/dev/null; then
     echo 'Git found'
-    git clone --depth 1 $GIT_MACOS_REPO $INSTALL_MACOS_PATH
-    git clone -b master --depth 1 --single-branch $GIT_SKM_REPO $INSTALL_SKM_PATH
+    git clone --depth 1 $GIT_MACOS_REPO "${INSTALL_MACOS_PATH}"
+    git clone -b master --depth 1 --single-branch "${GIT_SKM_REPO $INSTALL_SKM_PATH}"
   else
     echo 'Git not found, can''t install splashkit. Please run xcode developer command.'
 fi
 
 # Unzip the SKM app.
-unzip ~/.splashkit/skm/mac-build/skm.zip -d ~/.splashkit/skm/mac-build > ~/.splashkit/install.log
+unzip "$INSTALL_SKM_PATH/mac-build/skm.zip" -d "${INSTALL_PATH}/mac-build" > "${FOLDER_PATH}/install.log"
 
 # Add SKM app to path without needing sudo
 echo "export PATH=\""$HOME/.splashkit/skm/mac-build/skm.app/Contents/MacOs:$PATH\""" >> ~/.bash_profile
