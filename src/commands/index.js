726c10e68bf2b61229073b64a0431cfc16e05e56
Jake Renzella <j@kerenzella.com>
2016-10-20 15:22:37 +1100

    FIX: Revert back removing path require

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,11 +1,9 @@
 const fs = require('fs')
-const path = require('path')
 
 let commandDir = __dirname
 
-// Test this map
 let cmds = fs.readdirSync(commandDir)
              .filter(cmd => ['index.js', 'compilers'].indexOf(cmd) === -1)
-             .map(cmd => [cmd.slice(0, -3), require(path.resolve(__dirname, '/', cmd))])
+             .map(cmd => [cmd.slice(0, -3), require(__dirname + '/' + cmd)])
 
 module.exports = new Map(cmds)
