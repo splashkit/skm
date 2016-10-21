e8e18273985e3f07bf5325d89e1197b88a02c7ee
Jake Renzella <j@kerenzella.com>
2016-10-21 13:41:00 +1100

    FIX: Remove echose from git commands

--- mac-build/mac-install.sh
+++ mac-build/mac-install.sh
@@ -22,8 +22,8 @@ fi
 # Clone the repos if git is found.
 if which git >/dev/null; then
     echo 'Git found'
-   echo  git clone --depth 1 $GIT_MACOS_REPO "${INSTALL_PATH}"
-    echo git clone -b master --depth 1 --single-branch $GIT_SKM_REPO "${INSTALL_SKM_PATH}"
+    git clone --depth 1 $GIT_MACOS_REPO "${INSTALL_PATH}"
+    git clone -b master --depth 1 --single-branch $GIT_SKM_REPO "${INSTALL_SKM_PATH}"
   else
     echo 'Git not found, can''t install splashkit. Please run xcode developer command.'
 fi
@@ -32,15 +32,12 @@ fi
 echo "unzipping"
 unzip "$INSTALL_SKM_PATH/mac-build/skm.zip" -d "$INSTALL_SKM_PATH/mac-build" > "$FOLDER_PATH/install.log"
 
-
 # Add SKM app to path without needing sudo
-
 echo "export PATH=\""$HOME/.splashkit/skm/mac-build/skm.app/Contents/MacOs:\$PATH\""" >> ~/.bash_profile
 
 if [ -f ~/.zshrc ]; then
     echo "export PATH=\""$HOME/.splashkit/skm/mac-build/skm.app/Contents/MacOs:\$PATH\""" >> ~/.zshrc
 fi
-
 export PATH=\""$HOME/.splashkit/skm/mac-build/skm.app/Contents/MacOs:\$PATH\""
 
 echo "SplashKit Successfully installed! Please restart your terminal..."
