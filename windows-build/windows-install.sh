f4b7e37b1ef0fca3c0ddfefc27917650f0600e03
Jake Renzella <j@kerenzella.com>
2016-10-24 16:58:13 +1100

    FIX: Add to path each lib

--- windows-build/windows-install.sh
+++ windows-build/windows-install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-GIT_SKM_REPO=https://github.com/jakerenzella/skm
 GIT_WINDOWS_REPO=https://github.com/splashkit/splashkit-windows
 
 HOME_PATH=~
-FOLDER_PATH="${HOME_PATH}/.splashkit"
-INSTALL_PATH="${FOLDER_PATH}/splashkit-windows"
-INSTALL_SKM_PATH="${FOLDER_PATH}/skm"
+INSTALL_PATH="${HOME_PATH}/.splashkit"
 
 if [[ $MSYS2_PATH_TYPE != 'inherit' ]]; then 
     setx MSYS2_PATH_TYPE inherit
     echo Please restart your terminal and rerun this script.
 else
-    # echo "export PATH=\""$HOME/.splashkit/splashkit-windows/lib:\$PATH\""" >> ~/.bash_profile
-    export PATH="\"$HOME/.splashkit/splashkit-windows/lib\":$PATH"
-
     # Get tools if not installed
     command -v clang >/dev/null 2>&1 || { echo "clang not found, Installing clang." >&2; pacman -S mingw-w64-x86_64-clang mingw-w64-i686-clang --noconfirm;}
     command -v git >/dev/null 2>&1 || { echo "git not found, Installing git." >&2; pacman -S git --noconfirm;}
@@ -23,15 +17,11 @@ else
     echo git clone --depth 1 $GIT_WINDOWS_REPO "${INSTALL_PATH}"
     git clone --depth 1 $GIT_WINDOWS_REPO "${INSTALL_PATH}"
 
-    echo git clone -b master --depth 1 --single-branch $GIT_SKM_REPO "${INSTALL_SKM_PATH}"
-    git clone -b master --depth 1 --single-branch $GIT_SKM_REPO "${INSTALL_SKM_PATH}"
+    export PATH="$HOME/.splashkit/lib:$PATH"
+    export PATH="$HOME/.splashkit/lib/win32:$PATH"
+    export PATH="$HOME/.splashkit/lib/win64:$PATH"
 
-    # Unzip the SKM app.
-    # unzip "$INSTALL_SKM_PATH/windows-build/skm.zip" -d "${INSTALL_PATH}/windows-build" > "${FOLDER_PATH}/install.log"
-
-    # Shouldn't this just use the Linux build?
-    # Add SKM app to path
-    # echo "export PATH=\""$HOME/.splashkit/skm/windows-build/skm.app/Contents/windows:\$PATH\""" >> ~/.bash_profile
-    export PATH="\"$HOME/.splashkit/skm/windows-build/skm-win32-x64\":$PATH"
-    ${SYSTEMROOT}/system32/setx.exe PATH $PATH
+    export PATH="$HOME/.splashkit/skm-win32-x64:$PATH"
+    export PATH="$HOME/.splashkit/skm-win32-ia32:$PATH"
+    setx PATH "$PATH"
 fi
