16d1bd68928a6a03a502ab4bf6fe2b77be5b2656
Jake Renzella <j@kerenzella.com>
2017-02-14 11:23:38 +1100

    FIX: Exit out of OS detection blocks

--- install-scripts/skm-install.sh
+++ install-scripts/skm-install.sh
@@ -4,16 +4,21 @@ if [[ `uname` == Darwin ]]; then
   echo "installing SplashKit for MacOS" 
   bash <(curl -s https://raw.githubusercontent.com/splashkit/skm/master/install-scripts/mac-install.sh)
   echo "finished installing SplashKit for MacOS"
+  exit
 fi
 
 if [[ `uname` == MINGW* ]]; then
   echo "installing SplashKit for Windows MingW"
   bash <(curl -s https://raw.githubusercontent.com/splashkit/skm/master/install-scripts/windows-install.sh)
+  echo "finished installing SplashKit for Windows (MINGW)"
+  exit
 fi
 
 if [[ `uname` == Linux ]]; then
   echo "installing SplashKit for Linux"
   bash <(curl -s https://raw.githubusercontent.com/splashkit/skm/master/install-scripts/linux-install.sh)
+  echo "finished installing SplashKit for Linux"
+  exit
 fi
 
 echo "SplashKit is not supported on this Operating System. If using Windows, ensure you install SKM in a MinGW Terminal."
