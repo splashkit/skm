40c1b644bd3b14dc0842a7d9360b9574dc130d6a
Jake Renzella <j@kerenzella.com>
2017-02-16 10:23:15 +1100

    QUALITY: Remove clang++ install

--- install-scripts/mac-install.sh
+++ install-scripts/mac-install.sh
@@ -4,7 +4,6 @@ GIT_MACOS_REPO=https://github.com/splashkit/splashkit-macos
 HOME_PATH=~
 INSTALL_PATH="${HOME_PATH}/.splashkit"
 
-command -v clang++ >/dev/null 2>&1 || { echo "Developer tools not installed, please run: \"xcode-select --install\" in the terminal and then rerun this script." >&2; exit;}
 command -v git >/dev/null 2>&1 || { echo "Developer tools not installed, please run: \"xcode-select --install\" in the terminal and then rerun this script." >&2; exit;}
 
 git clone --depth 1 --branch master $GIT_MACOS_REPO "${INSTALL_PATH}"
