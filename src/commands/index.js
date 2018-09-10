79b93aca9a1344780546ec6f552d7f586c2c64a4
Andrew Cain <macite@gmail.com>
2018-09-10 14:24:07 +1000

    NEW: Initial commit of changes to bash skm

--- src/commands/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-let commandDir = __dirname
-
-let cmds = fs.readdirSync(commandDir)
-             .filter(cmd => ['index.js', 'compilers'].indexOf(cmd) === -1)
-             .map(cmd => [cmd.slice(0, -3), require(commandDir + '/' + cmd)])
-
-module.exports = new Map(cmds)
--- src/gui/index.js
+++
@@ -1,9 +0,0 @@
-const docs = require('../commands/docs')
-
-const execute = function (argv, callback) {
-  docs.execute(argv, callback)
-}
-
-module.exports = {
-  execute: execute
-}
