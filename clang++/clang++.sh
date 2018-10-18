57690534722e3cf32cb205ee7cd80a9b25a21031
Andrew Cain <macite@gmail.com>
2018-10-18 16:18:41 +1100

    FIX: Ensure we mark windows c++ as mwindows

--- clang++/clang++.sh
+++ clang++/clang++.sh
@@ -9,7 +9,7 @@ DYLIB_PATH=`cd "$APP_PATH"/../lib; pwd`
 
 if [ "`uname -o 2>>/dev/null`" = "Msys" ]; then
     #g++ -g -Wall -std=c++14 -L"${APP_PATH}/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" "$APP_PATH/src/*.cpp" $*
-    g++ -g -Wall -Wl,--as-needed -std=c++14 -L"${APP_PATH}/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${APP_PATH}/lib/win64/libSplashKitCpp.a"
+    g++ -mwindows -g -Wall -Wl,--as-needed -std=c++14 -L"${APP_PATH}/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${APP_PATH}/lib/win64/libSplashKitCpp.a"
 else
     clang++ -g -Wall -std=c++14 -L"$DYLIB_PATH" -lSplashKit -L"${APP_PATH}/lib" -lSplashKitCpp -I "${APP_PATH}/include" -rpath @loader_path -rpath "$DYLIB_PATH" -rpath /usr/local/lib $*
 fi
