b8689f882019ca6410aeea5f67163bc202558d6d
Andrew Cain <acain@swin.edu.au>
2019-01-24 22:39:57 +1100

    FIX: Creation of new c++ projects on windows

--- new/c++/skm_new_c++.sh
+++ new/c++/skm_new_c++.sh
@@ -7,18 +7,6 @@ SKM_PATH=`cd "$APP_PATH/../.."; pwd`
 
 source "${SKM_PATH}/tools/set_sk_env_vars.sh"
 
-INCLUDE_PATH=`cd "$SKM_PATH/clang++/include"; pwd`
-
-if [ ! -d include ]; then
-    mkdir include
-else
-    if [ -e include/splashkit ]; then
-        rm -rf include/splashkit
-    fi
-fi
-
-ln -f -s "${INCLUDE_PATH}" ./include/splashkit
-
 if [ "$IS_WINDOWS" = true ]; then
     cp -r -n "${APP_PATH}/files/" -T .
 else
