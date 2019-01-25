f3fc4068c90212f757776b2cfa840931e0065758
Andrew Cain <macite@gmail.com>
2019-01-25 15:02:47 +1100

    FIX: Correct issue with python copy path

--- new/python3/skm_new_python3.sh
+++ new/python3/skm_new_python3.sh
@@ -7,10 +7,10 @@ SKM_PATH=`cd "$APP_PATH/../.."; pwd`
 
 source "${SKM_PATH}/tools/set_sk_env_vars.sh"
 
-if [ "$IS_WINDOWS" = true ]; then
-    cp -r -n "${APP_PATH}/files/" -T .
-else
+if [ "$SK_OS" = "macos" ]; then
     cp -r -n "${APP_PATH}/files/" .
+else
+    cp -r -n "${APP_PATH}/files/" -T .
 fi
 
 "$SKM_PATH/fix/python3/skm_fix_python3.sh"
