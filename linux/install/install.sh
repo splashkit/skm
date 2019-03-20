1c1d66b8f0081d4fb66884191b7382a619add500
Rodney Pilgrim <rodneyp290@gmail.com>
2019-03-20 23:20:45 +1100

    REFACTOR: Merge `skm linux install` script usage into `skm linux help`, and remove call to skm update

--- linux/install/install.sh
+++ linux/install/install.sh
@@ -7,15 +7,6 @@ SKM_PATH=`cd "$APP_PATH/../.."; pwd`
 
 source "${SKM_PATH}/tools/set_sk_env_vars.sh"
 
-if ( echo "${*}" | grep -E '(-h|--help)\b' ) ; then
-  echo "Usage: skm linux install [-h|--help] [--no-os-detect] [--no-update]"
-  echo "Options:"
-  echo "    -h|--help       Display this usage messeage"
-  echo "    --no-os-detect  Bypass OS detection"
-  echo "    --no-update     Skip skm update"
-  exit 0;
-fi
-
 if [ "$SK_OS" = "linux" ] || ( echo "${*}" | grep '\-\-no-os-detect' ); then
   : # All good - no op and continue
 elif [ "$SK_OS" = "macos" ] || [ "$SK_OS" = "win64" ] || [ "$SK_OS" = "win32" ]; then
@@ -25,14 +16,6 @@ else
     echo "Unable to detect operating system..."
     echo "If you are running linux, you can bypass os detection with the '--no-os-detect' flag."
     exit 1
-fi
-
-if ( echo "${*}" | grep -v '\-\-no-update' ) ; then
-  echo "Updating splashkit headers..."
-  skm update
-  if [ $? -ne 0 ]; then
-    exit $?
-  fi
 fi
 
 echo "Configuring SplashKit"
