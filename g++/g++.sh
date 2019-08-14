7d525c84aa592f1bc40e62087293cd34c3f092de
Andrew Cain <macite@gmail.com>
2019-08-14 16:29:39 +1000

    FIX: Ensure no warning on sign compare

--- g++/g++.sh
+++ g++/g++.sh
@@ -7,15 +7,16 @@ SKM_PATH=`cd "$APP_PATH/.."; pwd`
 
 source "${SKM_PATH}/tools/set_sk_env_vars.sh"
 
+CPP_OPTIONS="-std=c++14 -g -Wall -Wno-sign-compare"
+
 if [ "$SK_OS" = "win32" ]; then
-    g++ -mwindows -g -Wall -Wl,--as-needed -std=c++14 -L"${SKM_PATH}/clang++/lib/win32" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win32 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${SKM_PATH}/clang++/lib/win32/libSplashKitCpp.a"
+    g++ -mwindows $CPP_OPTIONS -Wl,--as-needed -L"${APP_PATH}/lib/win32" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win32 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${APP_PATH}/lib/win32/libSplashKitCpp.a"
 elif [ "$SK_OS" = "win64" ]; then
-    #g++ -g -Wall -std=c++14 -L"${SKM_PATH}/clang++/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" "$APP_PATH/src/*.cpp" $*
-    g++ -mwindows -g -Wall -Wl,--as-needed -std=c++14 -L"${SKM_PATH}/clang++/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${SKM_PATH}/clang++/lib/win64/libSplashKitCpp.a"
+    g++ -mwindows $CPP_OPTIONS -Wl,--as-needed -L"${APP_PATH}/lib/win64" -static-libstdc++ -static-libgcc -lSplashKit -Wl,-Bstatic -lstdc++ -lpthread -I ${APP_PATH}/include -I ${APP_PATH}/../ -L ${SKM_PATH}/lib/win64 -I "$APP_PATH/src" -I "$APP_PATH/win_include" $* "${APP_PATH}/lib/win64/libSplashKitCpp.a"
 elif [ "$SK_OS" = "macos" ]; then
-    g++ -g -Wall -std=c++14 -L"$DYLIB_PATH" -lSplashKit -L"${SKM_PATH}/clang++/lib" -lSplashKitCpp -I "${APP_PATH}/include" -rpath @loader_path -rpath "$DYLIB_PATH" -rpath /usr/local/lib $*
+    g++ $CPP_OPTIONS -L"$DYLIB_PATH" -lSplashKit -L"${APP_PATH}/lib/macos" -lSplashKitCpp -I "${APP_PATH}/include" -rpath @loader_path -rpath "$DYLIB_PATH" -rpath /usr/local/lib $*
 elif [ "$SK_OS" = "linux" ]; then
-    g++ -g -Wall -std=c++14 -L"$DYLIB_PATH" -lSplashKit -L"${APP_PATH}/lib/linux" -I ${APP_PATH}/include -Wl,-rpath=$ORIGIN -Wl,-rpath="${DYLIB_PATH}" -Wl,-rpath=/usr/local/lib $* -lSplashKitCPP
+    g++ $CPP_OPTIONS -L"$DYLIB_PATH" -lSplashKit -L"${APP_PATH}/lib/linux" -I ${APP_PATH}/include -Wl,-rpath=$ORIGIN -Wl,-rpath="${DYLIB_PATH}" -Wl,-rpath=/usr/local/lib $* -lSplashKitCPP
 else
     echo "Unable to detect operating system..."
     exit 1
