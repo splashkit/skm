67d8ef5d378cb249e85a71714539ea6c2220dfa9
Andrew Cain <macite@gmail.com>
2019-08-15 10:02:07 +1000

    ENHANCE: Update message in install script for linux

--- install-scripts/skm-install.sh
+++ install-scripts/skm-install.sh
@@ -73,29 +73,32 @@ if [[ `uname` = MINGW64* ]] || [[ `uname` = MSYS* ]]; then
 fi
 
 if [[ `uname` = Linux ]]; then
-    # Add SKM app to path without needing sudo
-    echo "SKM and SplashKit depends on the following libraries:
-    * sdl2 development library
-    * sdl2 gfx development library
-    * sdl2 mixer development library
-    * sdl2 ttf development library
-    * sdl2 net development library
-    * sdl2 image development library
-    * ncurses development library
-    * png development library
-    * curl4 openssl development library
-    * bz2 development library
-    * flac, libvorbis libmikmod development libraries
-    * webp development library
-    * freetype development library
-    * CMAKE
+    #     # Add SKM app to path without needing sudo
+    #     echo "SKM and SplashKit depends on the following libraries:
+    #     * sdl2 development library
+    #     * sdl2 gfx development library
+    #     * sdl2 mixer development library
+    #     * sdl2 ttf development library
+    #     * sdl2 net development library
+    #     * sdl2 image development library
+    #     * ncurses development library
+    #     * png development library
+    #     * curl4 openssl development library
+    #     * bz2 development library
+    #     * flac, libvorbis libmikmod development libraries
+    #     * webp development library
+    #     * freetype development library
+    #     * CMAKE
 
-    Please ensure these dependencies are installed using your package manager.
-    For example:
+    #     Please ensure these dependencies are installed using your package manager.
+    #     For example:
 
-    sudo apt install libsdl2-dev libsdl2-gfx-dev libsdl2-mixer-dev libsdl2-ttf-dev libsdl2-net-dev libsdl2-image-dev libncurses-dev libpng-dev libcurl4-openssl-dev libbz2-dev libflac-dev libvorbis-dev libmikmod-dev libwebp-dev libfreetype6-dev cmake
+    #     sudo apt install libsdl2-dev libsdl2-gfx-dev libsdl2-mixer-dev libsdl2-ttf-dev libsdl2-net-dev libsdl2-image-dev libncurses-dev libpng-dev libcurl4-openssl-dev libbz2-dev libflac-dev libvorbis-dev libmikmod-dev libwebp-dev libfreetype6-dev cmake
 
-"
+    # "
+
+    echo "Run the following to install the necessary native libraries, compile and install splashkit:"
+    echo "${bold}skm linux install${normal}"
 fi
 
 find "${INSTALL_PATH}" -name "*.sh" -exec chmod a+x "{}" \;
