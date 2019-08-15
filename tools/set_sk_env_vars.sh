39bf4a77d30f77ffd20df0f9e2a7dad5293dc96b
Andrew Cain <macite@gmail.com>
2019-08-15 13:33:07 +1000

    FIX: Add message on missing linux library

--- tools/set_sk_env_vars.sh
+++ tools/set_sk_env_vars.sh
@@ -32,5 +32,8 @@ elif [ $SK_OS = "macos" ]; then
     export IS_WINDOWS=false
 else
     export DYLIB_PATH=`cd "$SKM_PATH/lib/linux"; pwd` 2>>/dev/null
+    if [ $? -neq 0 ]; then
+        echo "Unable to locate SplashKit library - please run ${bold}skm linux install${normal}"
+    else
     export IS_WINDOWS=false
 fi
