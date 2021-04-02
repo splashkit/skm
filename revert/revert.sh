ff3fad40bbd47b1b9d2e735e6052503d9ed3f97b
Andrew Cain <macite@gmail.com>
2021-04-02 14:36:54 +1100

    FIX: Convert $1 to int in revert

--- revert/revert.sh
+++ revert/revert.sh
@@ -3,7 +3,7 @@
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd`
 
-if [ "$1" -eq 1 ]; then
+if [ "$($1)" -eq 1 ]; then
     cd "$APP_PATH"
     git fetch --unshallow
     git checkout 576f7038e6c1a53b07c8b27cb7911c2271d30c87
