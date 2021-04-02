c682216b54ecc1ee73cc08218ed78d177bc63fbf
Andrew Cain <macite@gmail.com>
2021-04-02 14:31:49 +1100

    ENHANCE: Add revert command to allow reverting to older versions

---
+++ revert/revert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
+APP_PATH=`cd "$APP_PATH"; pwd`
+
+if [ $1 -eq 1 ]; then
+    cd "$APP_PATH"
+    git fetch --unshallow
+    git checkout 576f7038e6c1a53b07c8b27cb7911c2271d30c87
+else
+    "$APP_PATH/help.sh"
+    exit 1
+fi
