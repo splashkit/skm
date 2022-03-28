4dd0ff9e3d3bdcde04c475f795828a2bcc52fb28
talzid <101184078+talzid@users.noreply.github.com>
2022-03-28 20:44:15 +1100

    ADD: uninstall script
    Useful for team completing install upgrades

---
+++ uninstall/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
+APP_PATH=`cd "$APP_PATH"; pwd`
+
+HOME_PATH=~
+INSTALL_PATH="${HOME_PATH}/.splashkit"
+
+rm -rf ${INSTALL_PATH}
