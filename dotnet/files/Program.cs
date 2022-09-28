dcd420b07770157b08e3885343ed661c845f7820
Olivia McKeon <ordmckeon@gmail.com>
2022-09-28 18:32:20 +1000

    ADD: Added namespace lines to Program.cs file
    This will allow for easier changing of the namespace to the folder name in the skm_fix_dotnet.sh file

--- dotnet/files/Program.cs
+++ dotnet/files/Program.cs
@@ -1,10 +1,13 @@
 using System;
 using SplashKitSDK;
 
-public class Program
+namespace SkmProject
 {
-    public static void Main()
+    public class Program
     {
+        public static void Main()
+        {
 
+        }
     }
 }
