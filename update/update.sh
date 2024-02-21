afca14346968b91896f4a862dc72f8469a18bf60
Andrew Cain <macite@gmail.com>
2024-02-22 06:58:07 +1100

    Fix path in update script

--- update/update.sh
+++ update/update.sh
@@ -3,6 +3,8 @@
 APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=`cd "$APP_PATH"; pwd`
 
+SKM_PATH=`cd "$APP_PATH/.."; pwd`
+
 source "${SKM_PATH}/tools/set_sk_env_vars.sh"
 
 cd "$APP_PATH"
