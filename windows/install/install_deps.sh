547c8f0b4bf0bd89731d4b7075e59959778a7697
omckeon <ordmckeon@gmail.com>
2024-11-14 22:32:22 +1100

    Fix windows install script error

--- windows/install/install_deps.sh
+++ windows/install/install_deps.sh
@@ -8,7 +8,7 @@ to_upper () {
 }
 
 detect_msys2_shell () {
-    if [ "$MSYSTEM" = "MINGW64" || "$MSYSTEM" = "CLANG64" || "$MSYSTEM" = "CLANGARM64" ]; then
+    if [ "$MSYSTEM" = "MINGW64" ] || [ "$MSYSTEM" = "CLANG64" ] || [ "$MSYSTEM" = "CLANGARM64" ]; then
     	export DISTRO_ID=$MSYSTEM
     else
         echo "Unable to detect Windows version..."
@@ -22,17 +22,17 @@ install_deps () {
 	  echo Installing depencies with $1 method
 	  echo You are about to install the dependencies using the following command:
 	  echo   pacman -S --needed --noconfirm --disable-download-timeout mingw-w64-x86_64-clang mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-cmake mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-civetweb
-	  pacman -S --needed --noconfirm --disable-download-timeout mingw-w64-x86_64-clang mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-cmake mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-civetweb
+	  pacman -S --needed mingw-w64-x86_64-clang mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-cmake mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-civetweb
 	  ;;
   CLANG64 )
 	  echo Installing depencies with $1 method
 	  echo You are about to install the dependencies using the following command:
-	  pacman -S --needed --noconfirm --disable-download-timeout mingw-w64-clang-x86_64-clang mingw-w64-clang-x86_64-gcc mingw-w64-clang-x86_64-gdb mingw-w64-clang-x86_64-cmake mingw-w64-clang-x86_64-SDL2 mingw-w64-clang-x86_64-SDL2_gfx mingw-w64-clang-x86_64-SDL2_mixer mingw-w64-clang-x86_64-SDL2_image mingw-w64-clang-x86_64-SDL2_ttf mingw-w64-clang-x86_64-SDL2_net mingw-w64-clang-x86_64-civetweb
+	  pacman -S --needed mingw-w64-clang-x86_64-clang mingw-w64-clang-x86_64-gcc mingw-w64-clang-x86_64-gdb mingw-w64-clang-x86_64-cmake mingw-w64-clang-x86_64-SDL2 mingw-w64-clang-x86_64-SDL2_gfx mingw-w64-clang-x86_64-SDL2_mixer mingw-w64-clang-x86_64-SDL2_image mingw-w64-clang-x86_64-SDL2_ttf mingw-w64-clang-x86_64-SDL2_net mingw-w64-clang-x86_64-civetweb
 	  ;;
 	# CLANGARM64 )
 	#   echo Installing depencies with $1 method
 	#   echo You are about to install the dependencies using the following command:
-	#   echo   pacman -S --needed --noconfirm --disable-download-timeout mingw-w64-clang-aarch64-clang mingw-w64-clang-aarch64-gcc mingw-w64-clang-aarch64-gdb mingw-w64-clang-aarch64-cmake mingw-w64-clang-aarch64-SDL2 mingw-w64-clang-aarch64-SDL2_gfx mingw-w64-clang-aarch64-SDL2_mixer mingw-w64-clang-aarch64-SDL2_image mingw-w64-clang-aarch64-SDL2_ttf mingw-w64-clang-aarch64-SDL2_net mingw-w64-clang-aarch64-civetweb
+	#   echo   pacman -S --needed mingw-w64-clang-aarch64-clang mingw-w64-clang-aarch64-gcc mingw-w64-clang-aarch64-gdb mingw-w64-clang-aarch64-cmake mingw-w64-clang-aarch64-SDL2 mingw-w64-clang-aarch64-SDL2_gfx mingw-w64-clang-aarch64-SDL2_mixer mingw-w64-clang-aarch64-SDL2_image mingw-w64-clang-aarch64-SDL2_ttf mingw-w64-clang-aarch64-SDL2_net mingw-w64-clang-aarch64-civetweb
 	#   pacman -S --needed --noconfirm --disable-download-timeout mingw-w64-x86_64-clang mingw-w64-x86_64-gcc mingw-w64-x86_64-gdb mingw-w64-x86_64-cmake mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_gfx mingw-w64-x86_64-SDL2_mixer mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-SDL2_ttf mingw-w64-x86_64-SDL2_net mingw-w64-x86_64-civetweb
 	#   ;;
 	*)
