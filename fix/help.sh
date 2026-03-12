7d0478a9aaf8c467ec09fd61f33cd9750ef0fbeb
Andrew Cain <macite@gmail.com>
2026-03-12 11:04:45 +1100

    Ensure that skm commands work with double spaces
    echo $0 truncates spaces
    echo "$0" retains the spaces

--- fix/help.sh
+++ fix/help.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_PATH=`echo $0 | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
+APP_PATH=`echo "$0" | awk '{split($0,patharr,"/"); idx=1; while(patharr[idx+1] != "") { if (patharr[idx] != "/") {printf("%s/", patharr[idx]); idx++ }} }'`
 APP_PATH=$(cd "$APP_PATH" && pwd)
 
 SKM_PATH=`cd "$APP_PATH/.."; pwd`
